0e7f321d23927a08dae6ef04bc9f117a9a4e5fae
jpb <jpb.06@outlook.com>
2019-09-28 19:14:05 +0200

    Adding local storage helpers (clearLocalStorage & existsInLocalStorage)

--- src/logic/local.store.js
+++ src/logic/local.store.js
@@ -1,4 +1,6 @@
 const setInLocalStorage = (key, value) => localStorage.setItem(key, JSON.stringify(value));
 const getFromLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
+const existsInLocalStorage = (key) => localStorage.getItem(key) !== null;
+const clearLocalStorage = () => localStorage.clear();
 
-export { setInLocalStorage, getFromLocalStorage };
+export {setInLocalStorage, getFromLocalStorage, existsInLocalStorage, clearLocalStorage};
