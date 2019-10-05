c7d7b5f59485f22ec2d1a37dae658c59109479a2
jpb <jpb.06@outlook.com>
2019-10-05 15:58:57 +0200

    Adding the function to set daily duration

--- src/logic/api/daily.api.js
+++ src/logic/api/daily.api.js
@@ -1,23 +1,35 @@
-import TogetherApi from './setup/together.api';
-
-const addUnforeseenTicket = async (teamId, date, ticket) =>
-    await TogetherApi.Instance.post('daily/unforeseen/add', {
-        teamId: teamId,
-        date: date,
-        ticket: ticket
-    });
-
-const removeUnforeseenTicket = async (teamId, date, ticket) =>
-    await TogetherApi.Instance.post('daily/unforeseen/remove', {
-        teamId: teamId,
-        date: date,
-        ticket: ticket
-    });
+import TogetherApi, {ensureStatus} from './setup/together.api';
 
 const getDaily = async (teamId, date) =>
-    await TogetherApi.Instance.post('daily', {
+    ensureStatus(await TogetherApi.Instance.post('daily', {
         teamId: teamId,
         date: date
-    });
+    }));
 
-export {addUnforeseenTicket, removeUnforeseenTicket, getDaily};
+const reportDuration = async (teamId, date, durationIndicator) =>
+    ensureStatus(await TogetherApi.Instance.post('daily/setDuration', {
+        teamId: teamId,
+        date: date,
+        durationIndicator: durationIndicator
+    }));
+
+const addUnforeseenTicket = async (teamId, date, ticket) =>
+    ensureStatus(await TogetherApi.Instance.post('daily/unforeseen/add', {
+        teamId: teamId,
+        date: date,
+        ticket: ticket
+    }));
+
+const removeUnforeseenTicket = async (teamId, date, ticket) =>
+    ensureStatus(await TogetherApi.Instance.post('daily/unforeseen/remove', {
+        teamId: teamId,
+        date: date,
+        ticket: ticket
+    }));
+
+export {
+    getDaily,
+    reportDuration,
+    addUnforeseenTicket,
+    removeUnforeseenTicket
+};
