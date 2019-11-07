06bb8f056cb1237932f35bb8f42efab86298bbe3
jpb <jpb.06@outlook.com>
2019-11-07 18:12:35 +0100

    Deploy related jobs

---
+++ project-apparatus/deploy.commands.js
@@ -0,0 +1,45 @@
+const fs = require('fs-extra');
+const util = require('util');
+const GulpSSH = require('gulp-ssh');
+const exec = util.promisify(require('child_process').exec);
+
+const consoleUtil = require('./util/console.util.js');
+
+const settings = require('./private/private.config.js');
+const pckg = require('./../package.json');
+
+const main = {};
+
+main.sendFileToDeployServer = async function () {
+
+    consoleUtil.printHeader('Sending file to deploy server ...');
+
+    const {stdout, stderr} = await exec(`.\\project-apparatus\\pscp.exe -P ${settings.port} -l ${settings.user} -i ${settings.priPath} ./release/togetherfront_${pckg.version}.zip ${settings.user}@${settings.srvAddress}:${settings.destPath}`);
+    console.log('stdout:', stdout);
+    console.log('stderr:', stderr);
+
+    console.log('Done.');
+};
+
+main.deploy = function () {
+
+    consoleUtil.printHeader('Deploying ...');
+
+    let gulpSSH = new GulpSSH({
+        ignoreErrors: false,
+        sshConfig: {
+            host: settings.srvAddress,
+            port: settings.port,
+            username: settings.user,
+            privateKey: fs.readFileSync(settings.priPath)
+        }
+    });
+
+    return gulpSSH
+        .shell([`sudo ${settings.deployScriptPath}`], {filePath: `${pckg.version}_deploy.log`})
+        .on('ssh2Data', function (data) {
+            process.stdout.write(data.toString());
+        });
+};
+
+module.exports = main;
