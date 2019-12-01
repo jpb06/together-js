83d88508c3131defdd84894b7188fccfcd1123fe
jpb <jpb.06@outlook.com>
2019-12-01 09:42:55 +0100

    Factorizing client context init on successful login

--- src/logic/user.util.js
+++ src/logic/user.util.js
@@ -1,6 +1,28 @@
+import {LocalStorageKeys, setInLocalStorage} from "./local.store";
+
 const getInitials = (string) => {
     const initials = string.replace(/[^a-zA-Z- ]/g, "").match(/\b\w/g);
     return initials.join('').toUpperCase();
 };
 
-export {getInitials};
+const initializeLoggedUserContext = (authResult) => {
+    const fullName = `${authResult.user.firstName} ${authResult.user.lastName}`;
+    const initials = getInitials(fullName);
+
+    const user = {
+        ...authResult.user,
+        fullName,
+        initials
+    };
+
+    setInLocalStorage(LocalStorageKeys.token, authResult.token);
+    setInLocalStorage(LocalStorageKeys.expiration, authResult.expirationDate);
+    setInLocalStorage(LocalStorageKeys.user, user);
+
+    return user;
+};
+
+export {
+    getInitials,
+    initializeLoggedUserContext
+};
