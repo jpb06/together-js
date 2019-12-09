cf694fe167956da8103b8a29ae80c473132c849a
jpb <jpb.06@outlook.com>
2019-12-09 17:54:43 +0100

    Setting icons related with each timeline item type

--- src/UI/bricks/Timeline/step/TimeLineStep.js
+++ src/UI/bricks/Timeline/step/TimeLineStep.js
@@ -2,13 +2,21 @@ import React from "react";
 import TimelineStepContent from "./TimelineStepContent";
 import TimelineStepTitle from "./TimelineStepTitle";
 import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
+import CallMergeIcon from '@material-ui/icons/CallMerge';
+import GroupAddIcon from '@material-ui/icons/GroupAdd';
 
 const TimeLineStep = ({type, title, data}) => {
 
     const getTypeIcon = () => {
         switch (type) {
-            case 1:
+            case 1: // daily
                 return DirectionsRunIcon;
+            case 2: // team invite
+            case 4: // current user invite
+                return GroupAddIcon;
+            case 3: // team join request
+            case 5: // current user join request
+                return CallMergeIcon;
             default:
                 return undefined;
         }
